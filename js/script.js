6875a30de736abd5fe8ce1d16bb515b8018c794d
Ibrahim Yabagi Habib <55027665+ibraheemybg@users.noreply.github.com>
2023-03-24 12:09:04 +0100

    Slightly update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const toggle = document.querySelector('.toggle')
-const navigation = document.querySelector('.navigation')
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
 
-toggle.addEventListener('click', () => {
+toggle.addEventListener('click', function() {
   toggle.classList.toggle('active')
   navigation.classList.toggle('active')
-})
+});
